e0ab6e9270d2fd5242d9e8a02d793912a87a4326
pankaj692 <pankajk0692@gmail.com>
2021-04-27 22:23:45 +0530

    this is commit for seening the change

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -8,6 +8,8 @@ class TestOne(BaseClass):
 
     def test_e2e(self):
 
+        ## this is the test script for sbmailer login##
+
         button1 = self.driver.find_element_by_xpath('/html/body/app-root/app-header-component/nav/app-login-component/button')
         button1.click()
         print("click sucessfully")
@@ -16,23 +18,23 @@ class TestOne(BaseClass):
         # click continue with email
         button2 = self.driver.find_element_by_xpath('//*[@id="login"]/div/div[3]/button')
         button2.click()
-        print("click sucessfully")
+        print("click continue with email sucessfully")
 
         # enter the mail id
         Email = "don.don692@gmail.com"
         user_name = self.driver.find_element_by_xpath('//*[@id="login"]/div/form/div[2]/input')
         user_name.send_keys(Email)
-        print("Email entered")
+        print("Email entered sucessfully")
 
         # enter the password
         Password = "Pankaj692@"
         user_name = self.driver.find_element_by_xpath('//*[@id="login"]/div/form/div[3]/input')
         user_name.send_keys(Password)
-        print("Password entered")
+        print("Password entered sucessfully")
 
         # click the login button
         button3 = self.driver.find_element_by_xpath('//*[@id="login"]/div/form/div[5]/button')
         button3.click()
-        print("click sucessfully")
+        print("click login button sucessfully")
 
         print(self.driver.title)
--- tests/utilities/BaseClass.py
+++ tests/utilities/BaseClass.py
@@ -4,3 +4,4 @@ import pytest
 @pytest.mark.usefixtures("setup")
 class BaseClass:
     pass
+
